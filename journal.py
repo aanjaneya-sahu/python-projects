bb65b135f8e6fef0fca4906580e5bbfddd8427c3
aanjaneya-sahu <aanjaneyasahu@gmail.com>
2026-02-05 17:43:37 +0000

    Add files via upload

---
+++ journal.py
@@ -0,0 +1,32 @@
+file = open("example.txt","w")
+
+
+
+file.write("Hello World\n")
+
+file.write("This is a sample file")
+
+
+
+file.close()
+
+file = open("example.txt","r")
+
+current_content = file.read()
+
+print("File content:\n",current_content)
+
+file.close()
+
+with open("data.txt","w") as file:
+
+    file.write("Line 1:\n")
+
+with open("data.txt","a") as file:
+
+    file.write("Line 2:\n") 
+
+with open("data.txt","r") as file:
+
+    print(file.read())
+
